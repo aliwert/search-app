7af48c45140748fd4e13b23d5b9ed818b597ea95
aliwert <154356044+aliwert@users.noreply.github.com>
2024-04-07 23:17:36 +0300

    fixed some bugs

--- script.js
+++ script.js
@@ -1,63 +1,51 @@
-const formWrapper = document.querySelector(".form-wrapper");
-const form = document.querySelector("#form");
+const searchForm = document.querySelector(".search-form");
+const form = document.querySelector("#searchForm");
 const searchInput = document.querySelector("#searchInput");
-const buttonWrapper= document.querySelector(".button-wrapper");
+const buttonWrapper = document.querySelector(".button-wrapper");
 const searchButton = document.querySelector("#searchButton");
 const clearButton = document.querySelector("#clearButton");
-const imageListWrapper = document.querySelector(".imagelist-wrapper");
+const imageListContainer = document.querySelector(".imagelist-container");
 
 runEventListeners();
 
-function runEventListeners(){
-    form.addEventListener("submit" , search);
-    clearButton.addEventListener("click", clear);
+function runEventListeners() {
+  form.addEventListener("submit", search);
+  clearButton.addEventListener("click", clear);
 }
 
-function clear(){
-    searchInput.value="";
-    Array.from(imageListWrapper.children).forEach((child)=>child.remove())
-    // imageListWrapper.innerHTML="";
-    
+function clear() {
+  searchInput.value = "";
+  Array.from(imageListContainer.children).forEach((child) => child.remove());
 }
 
-function search(e){
-    const value = searchInput.value.trim();
-    // @RequestParam  - Spring- Rest APİ
-    fetch(`https://api.unsplash.com/search/photos?query=${value}`,{
-        method : "GET",
-        headers : {
-            Authorization : "Client-ID C-ZwMdVfZLUqf2EUV6lJeOB9k0_1CVGHsXaUfwJRamU"
-        }
+function search(e) {
+  const value = searchInput.value.trim();
+  fetch(`https://api.unsplash.com/search/photos?query=${value}`, {
+    method: "GET",
+    headers: {
+      Authorization: "Client-ID C-ZwMdVfZLUqf2EUV6lJeOB9k0_1CVGHsXaUfwJRamU",
+    },
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      Array.from(data.results).forEach((image) => {
+        addImageToUI(image.urls.small);
+      });
     })
-    .then((res)=> res.json())
-    .then((data)=>{
-       Array.from(data.results).forEach((image)=>{
-        // console.log(image.urls.small)
-        addImageToUI(image.urls.small)
-       })
-    })
-    .catch((err)=> console.log(err));
+    .catch((err) => console.log(err));
 
-
-    e.preventDefault();
+  e.preventDefault();
 }
 
+function addImageToUI(url) {
+  const div = document.createElement("div");
+  div.className = "card";
 
-function addImageToUI(url){
-    /*
- <div class="card">
-                <img src="" alt="">
-            </div>
-    */
-    console.log(imageListWrapper)
-    const div = document.createElement("div");
-    div.className="card";
+  const img = document.createElement("img");
+  img.setAttribute("src", url);
+  img.height = "400";
+  img.width = "400";
 
-    const img = document.createElement("img");
-    img.setAttribute("src",url);
-    img.height='400';
-    img.width='400';
-
-    div.append(img);
-    imageListWrapper.append(div);
+  div.append(img);
+  imageListContainer.append(div);
 }
